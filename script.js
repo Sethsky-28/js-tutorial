2f3839e31efdb51a58c10dd82de9561cae7d961d
Sethsky-28 <140408295+Sethsky-28@users.noreply.github.com>
2023-11-15 22:24:51 +0800

    arith

---
+++ script.js
@@ -0,0 +1,73 @@
+// let firstName = "Joel";
+// let pi = 3.14;
+
+// alert("Hi, " + firstName);
+// alert("How are you doing " + firstName + "?");
+// alert(firstName + ", The value of pi is " + pi);
+
+
+
+
+// let firstName = "Joel";
+// console.log("How are you " + firstName + "?");
+
+// let firstName = "Alenere";
+// let visitor = "David";
+// let gender = "she";
+// let food = "coffee";
+
+// console.log (firstName + ", the friendly neighbor, waved at " +
+// visitor + " as " +  gender + " walked by " + visitor + "'s house. " +
+// visitor + " smiled back and invited " +
+// firstName + " in a cup of " + food + ".");
+ 
+
+// console.log("How are you " + firstName + "?");
+
+//let num1 = "12";
+//let num2 = 2;
+//122
+
+//num1 = parseInt(num1)
+//console.log(num1 + num2);
+//console.log(parseInt(num1) + (num2));
+//14
+
+// let num1 = "12";
+// let num2 = 2;
+//console.log(num1 + num2);
+//14
+
+//let num1 = "15.53";
+//num1 = parseInt(num1)
+//console.log(num1);
+//15
+
+//let num1 = "15.99";
+//let num2 = 0.01;
+//num1 = parseFloat(num1);
+//console.log(num1 + num2);
+//16
+
+//let num1 = "15.99";
+//let num2 = 0.01;
+//num1 = parseFloat(num1);
+//console.log(num1 + num2);
+//15.990.01
+
+//let num1 = parseFloat(5);
+//console.log(num1);
+//5
+
+
+//let x = 12;
+//let y = 3;
+//console.log(x / y + x);
+//16
+
+//let x = 5;
+//let y = 10;
+//console.log((x+y)/x);
+//3
+
+ 
