9a2a19defe60a69c402a44b5ab99211936d91978
Sethsky-28 <140408295+Sethsky-28@users.noreply.github.com>
2023-11-21 22:02:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,15 @@
 // let firstName = "Joel";
 // console.log("How are you " + firstName + "?");
 
-let firstName = "Alenere";
-let visitor = "David";
-let gender = "she";
-let food = "coffee";
+// let firstName = "Alenere";
+// let visitor = "David";
+// let gender = "she";
+// let food = "coffee";
 
-console.log (firstName + ", the friendly neighbor, waved at " +
-visitor + " as " +  gender + " walked by " + visitor + "'s house. " +
-visitor + " smiled back and invited " +
-firstName + " in a cup of " + food + ".");
+// console.log (firstName + ", the friendly neighbor, waved at " +
+// visitor + " as " +  gender + " walked by " + visitor + "'s house. " +
+// visitor + " smiled back and invited " +
+// firstName + " in a cup of " + food + ".");
  
 
 // console.log("How are you " + firstName + "?");
@@ -70,4 +70,47 @@ firstName + " in a cup of " + food + ".");
 //console.log((x+y)/x);
 //3
 
- 
+// let num = 5;
+
+// num += 5;
+// console.log(num);
+
+
+// num *= 5;
+
+// let num = 5;
+// let x = 2;
+// num += (x ** 2);
+// console.log(num);
+
+
+// arithmetic prepix and postpix
+
+// let english = 90;
+// let math = 92;
+// let science = 91;
+// let hele = 90;
+// let mapeh = 93;
+
+// let average = (english + math + science + hele + mapeh)/5;
+// console.log("Average:" +  average);
+
+
+// string
+
+// let len = word.length;
+// console.log(len);
+
+
+// string indeces
+
+// let name = "Seth";
+// console.log(name[0]);
+// output: D
+
+// let word = "Joelle";
+// console.log(word[3]);
+// l
+
+let word = "Joelle";
+console.log(word[word.length - 1]);
